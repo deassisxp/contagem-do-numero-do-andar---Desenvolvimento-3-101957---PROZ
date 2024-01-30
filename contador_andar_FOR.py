b4e145e68539df0e0540d2fe4108f46d8c63ccd5
deassisxp <deassisxp@gmail.com>
2024-01-30 15:11:53 -0300

    first commit

---
+++ contador_andar_FOR.py
@@ -0,0 +1,8 @@
+num_andar = 20;
+
+for i in range(num_andar,-1,-1):
+    if(i == 0):
+        print("TÉRREO")
+    else:
+        if(i !=13):
+           print(f'{i}° ANDAR')
---
+++ contador_andar_WHILE.py
@@ -0,0 +1,9 @@
+num_andar = 20;
+
+while(num_andar > -1):
+    if(num_andar == 0):
+        print("TÉRREO")
+    else:
+        if(num_andar !=13):
+           print(f'{num_andar}° ANDAR')
+    num_andar -= 1;
